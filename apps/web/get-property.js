8909b22846d1962a758a358dada1de5b8b132e60
Inemon Finance <inemonifinance@gmail.com>
2026-08-30 13:10:03 +0100

    chore: clean up test files

--- apps/web/get-property.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const property = await prisma.property.findFirst();
-  console.log(property.id);
-}
-run().finally(() => prisma.$disconnect());
