b0532c6282c665bb8bcf3261406763315e565dab
Inemon Finance <inemonifinance@gmail.com>
2026-09-13 07:14:07 +0100

    chore: remove obsolete repair scripts

--- test-api-payload.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-const token = 'YOUR_API_TOKEN_HERE'; // wait, I don't need a token if I just run the equivalent code inside a script!
--- test-pull.js
+++
@@ -1,4 +0,0 @@
-const { NextRequest } = require('next/server');
-
-// I need to hit the API, but I can also just run the logic by importing it.
-// Or even better, I can start the dev server and curl it!
